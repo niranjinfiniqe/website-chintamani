202b3ccc2810648b229435e67ad9211d7f48ca50
ForamDesai <40829494+ForamDesai@users.noreply.github.com>
2022-07-05 17:19:36 +0530

    Remove extra images

--- View/adminPanel/header.php
+++ View/adminPanel/header.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
 
-    
+
 <head>
 
         <meta charset="utf-8" />
@@ -33,7 +33,7 @@
 
         <!-- Begin page -->
         <div id="layout-wrapper">
-            
+
             <header id="page-topbar">
                 <div class="navbar-header">
                     <div class="d-flex">
@@ -45,35 +45,22 @@
                                     <img src="assets/images/logo-sm.svg" alt="" height="24">
                                 </span>
                                 <span class="logo-lg">
-                                    <img src="assets/images/logo-sm.svg" alt="" height="24"> <span class="logo-txt">Minia</span>
+                                    <img src="assets/images/logo-sm.svg" alt="" height="24"> <span class="logo-txt">chintamani</span>
                                 </span>
                             </a>
 
-                            <a href="index.html" class="logo logo-light">
-                                <span class="logo-sm">
-                                    <img src="assets/images/logo-sm.svg" alt="" height="24">
-                                </span>
-                                <span class="logo-lg">
-                                    <img src="assets/images/logo-sm.svg" alt="" height="24"> <span class="logo-txt">Minia</span>
-                                </span>
-                            </a>
                     </div>
 
                         <button type="button" class="btn btn-sm px-3 font-size-16 header-item" id="vertical-menu-btn">
                             <i class="fa fa-fw fa-bars"></i>
                         </button>
 
-                
+
                     </div>
 
                     <div class="d-flex">
 
-
-                        <div class="dropdown d-inline-block">
-                            <button type="button" class="btn header-item right-bar-toggle me-2">
-                                <i data-feather="settings" class="icon-lg"></i>
-                            </button>
-                        </div>
+                    
 
                         <div class="dropdown d-inline-block">
                             <button type="button" class="btn header-item bg-soft-light border-start border-end" id="page-header-user-dropdown"
@@ -84,8 +71,6 @@
                                 <i class="mdi mdi-chevron-down d-none d-xl-inline-block"></i>
                             </button>
                             <div class="dropdown-menu dropdown-menu-end">
-                                <!-- item-->
-                                <a class="dropdown-item" href="apps-contacts-profile.html"><i class="mdi mdi-face-profile font-size-16 align-middle me-1"></i> Profile</a>
                                 <div class="dropdown-divider"></div>
                                 <a class="dropdown-item" href="index.php"><i class="mdi mdi-logout font-size-16 align-middle me-1"></i> Logout</a>
                             </div>
@@ -94,7 +79,7 @@
                     </div>
                 </div>
             </header>
-        
+
         </div>
 
             <!-- ========== Left Sidebar Start ========== -->
@@ -103,7 +88,7 @@
             <?php
                     include("sidebar.php");
             ?>
-            
+
 
 </body>
-</html>   
+</html>
--- View/adminPanel/sidebar.php
+++ View/adminPanel/sidebar.php
@@ -5,12 +5,6 @@
             <ul class="metismenu list-unstyled" id="side-menu">
                 <li class="menu-title" data-key="t-menu">Menu</li>
 
-                <!-- <li>
-                    <a href="home.php">
-                        <i data-feather="home"></i>
-                        <span data-key="t-dashboard">Dashboard</span>
-                    </a>
-                </li> -->
                 <li>
                     <a href="projects.php">
                         <i data-feather="grid"></i>
@@ -47,10 +41,6 @@
                         <span data-key="t-apps">Aminities</span>
                     </a>
                 </li>
-                
-
-
-              
 
             </ul>
 
